3c53639a5fe91a680e87930498ea2ab599f02a82
Anup Gc <anupgc@Anups-MacBook-Pro.local>
2021-02-17 10:24:31 +0000

    Fontawesome added

---
+++ src/helpers/pageItems.js
@@ -0,0 +1,34 @@
+const pageItems = [
+    {
+        id: 1,
+        type: 'supporting screen',
+        desc: 'start',
+        title: 'start'
+    },
+    {
+        id: 2,
+        type: 'welcome screen',
+        desc: 'first',
+        title: 'first'
+    },
+    {
+        id: 3,
+        type: 'welcome screen',
+        desc: 'second',
+        title: 'second'
+    },
+    {
+        id: 4,
+        type: 'welcome screen',
+        desc: 'third',
+        title: 'third'
+    },
+    {
+        id: 5,
+        type: 'supporting screen',
+        desc: 'end',
+        title: 'end'
+    }
+]
+
+export default pageItems
---
+++ src/helpers/users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        desc: 'John is a new customer for littledata. We want him to go through the full survey so that we can property setup his account'
+    },
+    {
+        id: 2,
+        name: 'Claire',
+        desc: 'Claire was invited by John to her account. We only want her to answer few questions and thats it'
+    },
+    {
+        id: 3,
+        name: 'Paul',
+        desc: 'Paul is a returning customer. Since he has already answered all the questions before, we can take him directly to the app'
+    }
+]
+
+export default pageItems
